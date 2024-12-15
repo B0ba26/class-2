30699beb9c53fca0254c619a76c4aa0e873839b6
B0ba26 <boba135t24@gmail.com>
2024-12-15 10:40:29 -0800

    Add files via upload

---
+++ activity 2.py
@@ -0,0 +1,26 @@
+print("welcome to the world of programing")
+print(54987456)
+print (1234567890987654211357908642)
+print('I am ',12, " years old")
+print("there are",50, "US states")
+#####  VARIABLES   ####
+
+name='Arya'
+age='12'
+hobby='basketball'
+likes='video games'
+
+print(name)
+print(age)
+print(hobby)
+print(likes)
+
+country=input("Enter the name of your country:\n")
+print("The enetered country is :", country)
+favorite_sport=input("enter your favortite sport:\n")
+print("The entered sport is :", favorite_sport)
+
+
+import keyword
+print("pyton keywords arep...:\n")
+print(keyword.kwlist)
